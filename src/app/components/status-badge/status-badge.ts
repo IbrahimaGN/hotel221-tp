13af224ee3824fd1dfcd3632f6e4180488c7737e
IbrahimaGN <gningkhalilo49@gmail.com>
2026-07-01 15:40:13 +0000

    V1

--- src/app/components/status-badge/status-badge.ts
+++ src/app/components/status-badge/status-badge.ts
@@ -1,15 +1,15 @@
 import { Component, input } from '@angular/core';
-import { NgClass } from '@angular/common'; // <-- ÉTAPE 1 : Ajouter l'importation
+import { NgClass } from '@angular/common'; 
 
 export type TransactionStatus = 'VALIDE' | 'EN_ATTENTE' | 'REJETE';
 
 @Component({
   selector: 'app-status-badge',
-  standalone: true, // Contrainte Obligatoire 
-  imports: [NgClass], // <-- ÉTAPE 2 : L'ajouter dans le tableau imports
+  standalone: true,  
+  imports: [NgClass], 
   templateUrl: './status-badge.html',
   styleUrls: ['./status-badge.css']
 })
 export class StatusBadgeComponent {
-  statut = input<TransactionStatus>('EN_ATTENTE'); // [cite: 7, 13]
+  statut = input<TransactionStatus>('EN_ATTENTE'); 
 }
